7a60fbd5539a6ad194020ff14a6f7a30300bff35
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-08-08 15:36:20 +0300

    Complete IO task.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String filePath = file.getAbsolutePath();
+        try {
+            return printFileWithBuffer(filePath);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Profile printFileWithBuffer(String path) throws FileNotFoundException {
+        Map<String, String> temp = new LinkedHashMap<>();
+        String[] tempArr = new String[2];
+            try (var br = new BufferedReader(new java.io.FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                tempArr = (line.split(": "));
+                temp.put(tempArr[0], tempArr[1]);
+            }
+        } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        return new Profile(temp.get("Name"), Integer.parseInt(temp.get("Age")), temp.get("Email"), Long.parseLong(temp.get("Phone")));
     }
 }
